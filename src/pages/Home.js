999c2ffce0df76e848bcecb1e4af4bab98bdcbfd
charLLLLLie <mega.joker00@gmail.com>
2021-05-13 11:53:29 +0300

    template

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,21 +1,26 @@
-import React from 'react'
-import Hero from '../components/Hero';
-import Banner from '../components/Banner';
-import {Link} from 'react-router-dom'
-import Servises from '../components/Servises';
-import FeaturedRooms from '../components/FeaturedRooms';
-export default function Home() {
-    return(<> <Hero>
-        <Banner title="Luxurios rooms" subtitle="deluxe starting at 249$">
-            <Link to='/rooms' className="btn-primary">
-                our rooms
-            </Link>
-
+import React from "react";
+import { Link } from "react-router-dom";
+import Hero from "../components/Hero";
+import Banner from "../components/Banner";
+import Services from "../components/Services";
+import FeaturedRooms from "../components/FeaturedRooms";
+const home = () => {
+  return (
+    <>
+      <Hero>
+        <Banner
+          title="luxurious rooms"
+          subtitle="deluxe rooms starting at $299"
+        >
+          <Link to="/rooms" className="btn-primary">
+            our rooms
+          </Link>
         </Banner>
-    </Hero>
-    <Servises/>
-    <FeaturedRooms/>
-
+      </Hero>
+      <Services />
+      <FeaturedRooms />
     </>
-    );
-}
+  );
+};
+
+export default home;
